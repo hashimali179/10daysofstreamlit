2aab2b1859b16718fd63640ae68761949147ef3d
Hashim Ali Zaidi <62440904+hashimali179@users.noreply.github.com>
2023-08-01 17:04:37 +0530

    Update penguin-ml-app.py

--- Day-8/penguin-ml-app.py
+++ Day-8/penguin-ml-app.py
@@ -40,11 +40,7 @@ else:
         return features
     input_df = user_input_features()
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
+
 
 # Encoding of ordinal features
 # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
